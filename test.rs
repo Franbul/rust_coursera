2264fa30fbc05c44ae54debd9f74b7c02a9f5652
FranBul <154807676+Franbul@users.noreply.github.com>
2023-12-25 19:32:55 +0000

    delete file

--- test.rs
+++ test.rs
@@ -7,11 +7,26 @@ fn average(numbers: &[i32]) -> f64 {
     (sum as f64) / (numbers.len() as f64)
 }   
 
+fn inverse_matrix(matrix: [[f64; 2]; 2]) -> Option<[[f64; 2]; 2]> {
+    let det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    if det == 0.0 {
+        None
+    } else {
+        Some([
+            [matrix[1][1] / det, -matrix[0][1] / det],
+            [-matrix[1][0] / det, matrix[0][0] / det],
+        ])
+    }
+}
+
 // Call the average function with somme numbers
 fn main() {
-    let numbers = [1, 2, 3, 4, 5];
-    let result = average(&numbers);
-    println!("The average is {}", result);
+    //let numbers = [1, 2, 3, 4, 5];
+    //let result = average(&numbers);
+    //println!("The average is {}", result);
+
+
+    
 }
 
 // Run: rustc test.rs
