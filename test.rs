5abe5deb0fe1173bc21778123477646364249110
FranBul <154807676+Franbul@users.noreply.github.com>
2023-12-25 18:46:15 +0000

    Add launch configuration and calculate average of numbers

--- test.rs
+++ test.rs
@@ -1,18 +1,21 @@
+// Create a function that calculate average of several numbers and return it
+fn average(numbers: &[i32]) -> f64 {
+    let mut sum = 0;
+    for num in numbers {
+        sum += num;
+    }
+    (sum as f64) / (numbers.len() as f64)
+}   
 
-
+// Call the average function with somme numbers
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    let numbers = [1, 2, 3, 4, 5];
+    let result = average(&numbers);
+    println!("The average is {}", result);
 }
 
-
-
-
-
-
-
-
+// Run: rustc test.rs
+// Run: ./test
+// Output: The average is 3
 
 
