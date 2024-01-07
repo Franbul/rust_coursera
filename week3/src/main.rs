6341753ad1592def07d49da9c1ee35034e8d2f37
FranBul <154807676+Franbul@users.noreply.github.com>
2024-01-07 17:13:04 +0000

    Add Person struct and full_name function

--- week3/src/main.rs
+++ week3/src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+struct Person {
+    first_name: String,
+    last_name: String,
+    age: u8,
+}
+
+fn full_name(person: &Person) -> String {
+    let mut full_name = person.first_name.clone();
+    full_name.push_str(" ");
+    full_name.push_str(&person.last_name);
+    full_name
+}
+
+fn exerice1() {
+    let myself = Person {
+        first_name: "John".to_string(),
+        last_name: "Doe".to_string(),
+        age: 30,
+    };
+    println!("{:?}", myself);
+
+    println!("My name is {}", full_name(&myself));
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Concatenation of full name from a struct
+    exerice1();
 }
