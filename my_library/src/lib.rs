8e5c4615149b2551e04f1efa9e7755ccfbb3a548
FranBul <154807676+Franbul@users.noreply.github.com>
2024-01-14 18:03:05 +0000

    Addition function and library documentation

--- my_library/src/lib.rs
+++ my_library/src/lib.rs
@@ -1,3 +1,38 @@
+/// # My Library
+///
+/// `my_library` est une bibliothèque qui fournit une fonction pour effectuer l'addition de deux nombres.
+
+/// Additionne deux nombres.
+///
+/// Cette fonction prend deux nombres de type `usize` et retourne leur somme.
+///
+/// # Exemples
+///
+/// ```
+/// use my_library::add;
+/// let result = add(2, 2);
+/// assert_eq!(result, 4);
+/// ```
+
+/// `add` - Additionne deux nombres.
+///
+/// # Paramètres
+///
+/// * `left`: Le premier nombre à additionner. Il doit être de type `usize`.
+/// * `right`: Le deuxième nombre à additionner. Il doit être de type `usize`.
+///
+/// # Retour
+///
+/// Retourne la somme de `left` et `right` sous forme de `usize`.
+///
+/// # Exemples
+///
+/// ```
+/// use my_library::add;
+/// let result = add(2, 2);
+/// assert_eq!(result, 4);
+/// ```
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -6,6 +41,9 @@ pub fn add(left: usize, right: usize) -> usize {
 mod tests {
     use super::*;
 
+    /// Teste la fonction `add`.
+    ///
+    /// Ce test vérifie que la fonction `add` retourne correctement la somme de deux nombres.
     #[test]
     fn it_works() {
         let result = add(2, 2);
--- week4/src/main.rs
+++ week4/src/main.rs
@@ -1,3 +1,33 @@
+//! # My Library
+//!
+//! `my_library` est une bibliothèque qui fournit des fonctionnalités pour effectuer des opérations arithmétiques de base.
+//!
+//! ## Exemples
+//!
+//! Voici un exemple d'utilisation de la fonction `add` de `my_library` :
+//!
+//! ```
+//! use my_library::add;
+//! let a: usize = 1;
+//! let b: usize = 2;
+//! assert_eq!(add(a, b), 3);
+//! ```
+//!
+//! ## Fonctions
+//!
+//! - `add`: Cette fonction prend deux nombres et retourne leur somme.
+
+use my_library;
+
+/// # Main
+///
+/// La fonction `main` est le point d'entrée de notre programme. Elle utilise la fonction `add` de `my_library` pour additionner deux nombres, puis imprime le résultat.
+///
+/// Ensuite, elle imprime "Hello, world!" à l'écran.
+
 fn main() {
+    let a: usize = 1;
+    let b: usize = 2;
+    println!("{}", my_library::add(a, b));
     println!("Hello, world!");
 }
